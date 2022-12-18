0eb9358f2c87a1c3e4a2f6300caf41b443d79565
Shaowei <11704698+zsw12abc@users.noreply.github.com>
2022-12-18 15:46:02 +1100

    Add boost

--- Assets/Driver.cs
+++ Assets/Driver.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,7 +6,9 @@ using UnityEngine;
 public class Driver : MonoBehaviour
 {
     [SerializeField] private float steerSpeed = 250f;
-    [SerializeField] private float moveSpeed = 10f;
+    [SerializeField] private float moveSpeed = 30f;
+    [SerializeField] private float slowSpeed = 20f;
+    [SerializeField] private float bootSpeed = 40f;
 
     // Start is called before the first frame update
     void Start()
@@ -19,5 +22,18 @@ public class Driver : MonoBehaviour
         float moveAmount = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
         transform.Rotate(0, 0, -steerAmount);
         transform.Translate(0, moveAmount, 0);
+    }
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        moveSpeed = slowSpeed;
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.tag == "Boost")
+        {
+            moveSpeed = bootSpeed;
+        }
     }
 }
